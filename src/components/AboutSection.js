3d275167d7ff5167b4a59fdc41f632d67c3dc78b
arakhm6 <arakhm6@gmail.com>
2021-04-30 02:09:17 -0400

    styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,29 +6,29 @@ import styled from "styled-components";
 const AboutSection = () => {
   return (
     <About>
-      <div className="description">
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque,
           consectetur?
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={waves} alt="waves" />
-      </div>
+      </Image>
     </About>
   );
 };
@@ -41,6 +41,28 @@ const About = styled.div`
   justify-content: space-between;
   padding: 5rem 10rem;
   color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 85vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
 `;
 
 export default AboutSection;
