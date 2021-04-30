6ad319e3f0bb298c3d6879629aeef5f5d531a9b0
arakhm6 <arakhm6@gmail.com>
2021-04-30 01:47:47 -0400

    about section styling

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from "react";
 import waves from "../img/waves.jpeg";
+//Styling
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
+    <About>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -27,8 +29,18 @@ const AboutSection = () => {
       <div className="image">
         <img src={waves} alt="waves" />
       </div>
-    </div>
+    </About>
   );
 };
+
+//Styled component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
 
 export default AboutSection;
