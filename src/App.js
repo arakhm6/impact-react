807a78d4367c92d1e145ba0328573dfe97c6dba6
arakhm6 <arakhm6@gmail.com>
2021-05-01 01:16:43 -0400

    react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+
+//Router
+import { Switch, Route } from "react-router-dom";
+
 //Global style
 import GlobalStyle from "./components/GlobalStyle";
-import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
